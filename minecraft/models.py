bdbf868e98f39f57eb309588aa376474017f0514
Stepan Tarabin <upconett@gmail.com>
2026-07-30 09:08:27 +0600

    feat: minecraft models

--- minecraft/models.py
+++ minecraft/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class MCServer(models.Model):
+    server_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    client_json_url = models.URLField()
+    client_json_size = models.BigIntegerField()
+    modded = models.BooleanField()
+
+    class Meta:
+        db_table = "mc_server"
+
+
+class MCModPack(models.Model):
+    modpack_id = models.IntegerField(primary_key=True)
+    server = models.ForeignKey(MCServer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    required = models.BooleanField()
+    url = models.URLField()
+    size = models.BigIntegerField()
+
+    class Meta:
+        db_table = "mc_modpack"
